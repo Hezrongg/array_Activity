7d6f26cb363950c4a17176af27fd1456f261a7d9
Hezrongg <hezrongg26@gmail.com>
2024-10-22 21:16:34 +0800

    initial commit

---
+++ arrayActivity.js
@@ -0,0 +1,11 @@
+// Favorite Singers
+let favSingers = ["Ed Sheeran", "Shawn Mendes", "Justin Bieber"];
+console.log("My first favorite singer is", favSingers[0]);
+
+// Favorite Numbers
+let favNumbers = ["06", "26", "20", "05"];
+let mixedArr = ["string", ["otherarray"], 123, true];
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
